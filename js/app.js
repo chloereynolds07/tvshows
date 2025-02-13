77174852c787191be5bcd6922cdf08e7c6ec65c4
chloereynolds07 <chloe@codingacademies.org>
2025-02-13 11:58:28 -0800

    did some javascript

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@
 class Shows {
 
     constructor() {
-        this.date = [
+        this.data = [
             {
                 id: 'cn001',
                 title: 'adventure time',
@@ -166,7 +166,89 @@ class Shows {
                 network: 'nickelodeon',
                 creator: 'janice burgess',
                 isFavorite: false
-            },
+            }
         ]
+
+        this.row = document.getElementById('row')
     }
+
+    // intializer
+    init() {
+        // this.buildCard(this.data[0])
+        this.loadCards(this.data)
+    }
+
+    // buildCard 
+    buildCard(obj) {
+        const row = this.row
+        
+
+        const column = document.createElement('div')
+        column.classList.add('col')
+
+        const card = document.createElement('div')
+        card.classList.add('card', 'h-100')
+        card.setAttribute('id', `card-${obj.id}`)
+
+        card.innerHTML = `
+            <div class="card-header">
+                <h2 class="card-heading text-capitalize">${obj.title}</h2>
+            </div>
+            <section class="card-body>
+                <p class="card-text">
+                    <span>${obj.yearStart.toDateString()}</span> - 
+                    <span>${obj.yearEnd.toDateString()}</span>
+                </p>
+                <p class="card-text">Episodes: ${obj.episodeCount}</p>
+                <p class="card-text">Seasons: ${obj.seasonCount}</p>
+                <p class="card-text text-capitalize">${obj.network}</p>
+                <p class="card-text text-capitalize">${obj.creator}</p>
+            </section>
+            <footer class="card-footer">
+                <button class="btn btn-dark text-capitalize favoriteBtns" id="${obj.id}">${obj.isFavorite ? 'unfavorite' : 'favorite'}
+                </button>
+                <span id="favorite">${obj.isFavorite ? '( •̀ ω •́ )✧' : '(┬┬﹏┬┬)'}</span>
+            </footer>
+        `
+
+        column.appendChild(card)
+        row.appendChild(column)
+    }
+
+    // loadCards
+    loadCards(arr) {
+        arr.forEach(item => this.buildCard(item))
+    }
+
+    // toggleFavorite
+    toggleFavorite(el, arr) {
+        
+        /**
+         * loop through array
+         *  if id of button === id of object in array, isFavorite  = !isFavorite
+         */
+
+        for (let item of arr) {
+            if (el.id == item.id) {
+                item.isFavorite = !item.isFavorite
+                console.log(item.isFavorite);
+                
+            }
+        }
+    }  
+
 }
+
+const action = new Shows()
+
+action.init()
+
+const buttons = document.querySelectorAll('.favoriteBtns')
+
+// console.log(buttons);
+
+buttons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        action.toggleFavorite(button, action.data)
+    })
+})
